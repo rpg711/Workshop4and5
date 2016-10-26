065fbc079216e6dd7e22fef7443d76ca9d145ee9
Tony Gao <zijiangao@umass.edu>
2016-10-25 20:37:09 -0400

    fb4

--- app/components/feed.js
+++ app/components/feed.js
@@ -2,12 +2,12 @@ import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
 export default class Feed extends React.Component {
-render() {
-  return (
-<div>
-<StatusUpdateEntry />
-<FeedItem />
-</div>
-)
-}
+  render() {
+    return (
+      <div>
+      <StatusUpdateEntry />
+      <FeedItem />
+      </div>
+    )
+  }
 }
